cdd7340b954398b0eb2c75a96f1fb420f121d67e
cookiemonster <dhrgevers@gmail.com>
2024-08-10 22:32:56 +0200

    Automated commit on 2024-08-10 22:32:56

--- feed/feed.sh
+++ feed/feed.sh
@@ -7,7 +7,7 @@ curl -s https://medium.com/feed/@victor_gevers -o feed.xml
 echo "<html><head><title>RSS Feed</title></head><body><h1>RSS Feed from Victor Gevers</h1><ul>" > feed.html
 
 # Use python to generate the feed.xml
-python feed.py
+python3 feed.py
 
 # Clean up
 rm feed.xml
